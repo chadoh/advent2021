18ab4dbbe8459ade80bc7c71e1b238e9a84f8bca
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-12-11 13:52:46 -0500

    aoc11.1: flashing octopuses

---
+++ 11/1.js
@@ -0,0 +1,40 @@
+const rows = require('fs').readFileSync(
+  require('path').join(__dirname, 'input.txt'),
+  'utf8'
+).split('\n').map(row => row.split('').map(Number))
+
+const STEPS = 100
+
+let toFlash = []
+
+function increase({ x, y }) {
+  if (x >= 0 && x < rows[0].length && y >= 0 && y < rows.length) {
+    rows[y][x]++
+    if (rows[y][x] === 10) {
+      toFlash.push([x, y])
+      for (let deltaX = -1; deltaX <= 1; deltaX++) {
+        for (let deltaY = -1; deltaY <= 1; deltaY++) {
+          if (!(deltaX === 0 && deltaY === 0)) {
+            increase({ x: x + deltaX, y: y + deltaY })
+          }
+        }
+      }
+    }
+  }
+}
+
+let flashCount = 0
+
+for (let i = 0; i < STEPS; i++) {
+  rows.forEach((row, y) => row.forEach((point, x) => {
+    increase({ x, y })
+  }))
+  flashCount += toFlash.length
+  toFlash.forEach(([x, y]) => rows[y][x] = 0)
+  toFlash = []
+}
+
+console.log({
+  rows: rows.map(row => row.join('')),
+  flashCount,
+})
